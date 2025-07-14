b1bccd57dfddb3b95749eebe50c886cca2306021
maguiat <magaliat16@gmail.com>
2025-07-14 11:00:03 -0300

    feat: modifica un producto existente (PUT /productos/:codigo )

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -3,12 +3,14 @@ const {
     getProducts, 
     getProductByCode,
     addProduct,
+    updateProduct,
      } = require("../controllers/productController")
 
 // Rutas
 router.get("/", getProducts)
 router.get("/:code", getProductByCode)
 router.post("/", addProduct)
+router.put("/:codigo", updateProduct)
 
 
 module.exports = router
